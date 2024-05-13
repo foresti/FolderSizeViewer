1b262e4e954f988886703e0a66ac158a0a639d46
Andrea Foresti <aforesti@worldbank.org>
2024-05-13 13:59:50 -0400

    Add project files.

---
+++ FolderSizeViewer/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace FolderSizeViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            FolderTree = new TreeView();
+            pBar = new ProgressBar();
+            SuspendLayout();
+            // 
+            // FolderTree
+            // 
+            FolderTree.AllowDrop = true;
+            FolderTree.Dock = DockStyle.Fill;
+            FolderTree.Location = new Point(0, 0);
+            FolderTree.Name = "FolderTree";
+            FolderTree.Size = new Size(1237, 695);
+            FolderTree.TabIndex = 0;
+            FolderTree.DragDrop += FolderTree_DragDrop;
+            FolderTree.DragEnter += FolderTree_DragEnter;
+            // 
+            // pBar
+            // 
+            pBar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pBar.Location = new Point(13, 617);
+            pBar.Name = "pBar";
+            pBar.Size = new Size(1211, 57);
+            pBar.TabIndex = 1;
+            pBar.Visible = false;
+            // 
+            // MainForm
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new SizeF(18F, 40F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1237, 695);
+            Controls.Add(pBar);
+            Controls.Add(FolderTree);
+            Font = new Font("Cascadia Mono", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "Folder Size Viewer";
+            TopMost = true;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TreeView FolderTree;
+        private ProgressBar pBar;
+    }
+}
---
+++ FolderSizeViewer/MainForm.cs
@@ -0,0 +1,173 @@
+﻿using System.Drawing.Design;
+using System.Xml.Linq;
+
+namespace FolderSizeViewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public int CountDir(string dirName)
+        {
+            var children = System.IO.Directory.GetDirectories(dirName);
+            int childrenCount = 0;
+            foreach (var child in children)
+            {
+                int childCount = CountDir(child);
+
+                childrenCount += childCount;
+            }
+
+            return childrenCount + 1;
+        }
+        int folderCount = 0;
+        int foldersDone = 0;
+        private void FolderTree_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] entries = e.Data.GetData(DataFormats.FileDrop) as string[];
+            string dirName = entries[0];
+            if (System.IO.Directory.Exists(dirName))
+            {
+                this.FolderTree.Nodes.Clear();
+                this.FolderTree.Enabled = false;
+
+                System.Threading.Thread t = new Thread(new ThreadStart(
+                    () =>
+                    {
+                        this.Invoke(() => this.Text = "Folder Size Viewer [counting directories...]");
+                        folderCount = CountDir(dirName);
+                        this.Invoke(() => this.Text = "Folder Size Viewer [getting sizes...]");
+                        foldersDone = 0;
+                        this.pBar.Invoke(() => pBar.Visible = true);
+                        var folderData = DoDir(dirName, true);
+                        this.FolderTree.Invoke(() => this.FolderTree.Nodes.Add(CreateLabel(folderData, folderData.Size, 0)));
+                        this.FolderTree.Invoke(() => AddChildrenToNode(this.FolderTree.Nodes[0], folderData, folderData.Size));
+                        this.pBar.Invoke(() => pBar.Visible = false);
+                        this.FolderTree.Invoke(() => this.FolderTree.Enabled = true);
+                        this.Invoke(() => this.Text = "Folder Size Viewer");
+                    }
+                ));
+                t.Start();
+            }
+        }
+
+        private static string CreateLabel(FolderData folderData, long totalSize, int maxNameLength)
+        {
+            double perc = System.Convert.ToDouble(folderData.Size) / System.Convert.ToDouble(totalSize);
+            string pBar = ProgressBar(perc * 100, ProgressBarMethod.shade, 20);
+            return $"{folderData.Name.PadRight(maxNameLength)} [{pBar}] ({(folderData.Size / 1000).ToString("#,##0")}kb - {folderData.NumberOfFiles.ToString("#,##0")} file{(folderData.NumberOfFiles == 0 ? "" : "s")})";
+        }
+
+        void AddChildrenToNode(TreeNode node, FolderData parent, long totalSize)
+        {
+            int maxNameLength = 0;
+            foreach (var child in parent.Children)
+            {
+                if (child.Name.Length > maxNameLength)
+                    maxNameLength = child.Name.Length;
+            }
+
+            foreach (var child in parent.Children)
+            {
+                var currentNode = node.Nodes.Add(CreateLabel(child, totalSize, maxNameLength));
+                if (child.Children.Count > 0)
+                {
+                    AddChildrenToNode(currentNode, child, totalSize);
+                }
+            }
+        }
+
+        public enum ProgressBarMethod
+        {
+            portion,
+            shade
+        }
+        public static string ProgressBar(double percentValue, ProgressBarMethod method, int width)
+        {
+            string returnString = "";
+
+            int numChars = System.Convert.ToInt32(Math.Floor(System.Convert.ToDouble(width) * (percentValue / 100)));
+            double blockValue = 100.0 / System.Convert.ToDouble(width);
+            double remainder = percentValue - blockValue * numChars;
+            string midChar = "";
+
+            switch (method)
+            {
+                case ProgressBarMethod.portion:
+                    if (remainder > 0.5 * blockValue)
+                        midChar = "▌";
+                    break;
+                case ProgressBarMethod.shade:
+                    if (remainder > 0.25 * blockValue)
+                        midChar = "░";
+                    if (remainder > 0.5 * blockValue)
+                        midChar = "▒";
+                    if (remainder > 0.75 * blockValue)
+                        midChar = "▓";
+                    break;
+            }
+
+            returnString = returnString.PadLeft(numChars, '█');
+            if (returnString.Length < width)
+                returnString += midChar;
+
+            returnString = returnString.PadRight(width, ' ');
+
+            return returnString;
+        }
+
+        FolderData DoDir(string dirName, bool top)
+        {
+            FolderData currentFolder = new FolderData();
+            var d = new System.IO.DirectoryInfo(dirName);
+            if (top)
+                currentFolder.Name = dirName;
+            else
+                currentFolder.Name = d.Name;
+
+            var children = System.IO.Directory.GetDirectories(dirName);
+            long childrenSize = 0;
+            long childrenCount = 0;
+            foreach (var child in children)
+            {
+                var childData = DoDir(child, false);
+                childrenSize += childData.Size;
+                childrenCount += childData.NumberOfFiles;
+                currentFolder.Children.Add(childData);
+            }
+            currentFolder.Children = currentFolder.Children.OrderBy(x => x.Size).Reverse().ToList();
+            currentFolder.Size = childrenSize;
+            currentFolder.NumberOfFiles = childrenCount;
+
+            var files = System.IO.Directory.GetFiles(dirName);
+            foreach (var file in files)
+            {
+                var f = new System.IO.FileInfo(file);
+                currentFolder.Size += f.Length;
+                currentFolder.NumberOfFiles++;
+            }
+            pBar.Invoke(() => pBar.Value = System.Convert.ToInt32((System.Convert.ToDouble(foldersDone) / System.Convert.ToDouble(folderCount)) * 100));
+
+            foldersDone++;
+            return currentFolder;
+        }
+
+        private void FolderTree_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+    }
+    public class FolderData
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public long NumberOfFiles { get; set; }
+        public List<FolderData> Children { get; set; } = new List<FolderData>();
+    }
+}
